85f04eb62da297d278541d11bdc1a5f053f8c167
separal <separal@crimson.ua.edu>
2024-12-10 14:20:36 -0600

    Bug Fixes

---
+++ Book_Haven/Models/BookClick/BookClick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Fall2024_Assignment3_separal.Models
+{
+    public class BookClick
+    {
+        public int ID { get; set; } // Primary Key
+        public int BookID { get; set; } // Foreign Key to the Book table
+        public DateTime ClickTime { get; set; } // Timestamp for the click
+
+        // Navigation property
+        public Book Book { get; set; }
+    }
+}
+
